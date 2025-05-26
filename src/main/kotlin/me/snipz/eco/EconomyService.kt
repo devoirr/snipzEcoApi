ca08a68ce1de1e4bcc54f8e0ec5322642c7b0c0b
devoirr <devoirr@users.noreply.github.com>
2025-05-27 01:19:23 +0300

    Switch EconomyService methods to use suspending functions.
    Updated methods to replace CompletableFuture with Kotlin's suspend functions for better coroutine support. This simplifies integrations with coroutine-based codebases while maintaining functionality.

--- src/main/kotlin/me/snipz/eco/EconomyService.kt
+++ src/main/kotlin/me/snipz/eco/EconomyService.kt
@@ -1,13 +1,12 @@
 package me.snipz.eco
 
 import java.util.UUID
-import java.util.concurrent.CompletableFuture
 
 interface EconomyService {
 
-    fun depositPlayer(player: UUID, currency: String, amount: Double): CompletableFuture<EconomyResponse>
-    fun withdrawPlayer(player: UUID, currency: String, amount: Double): CompletableFuture<EconomyResponse>
+    suspend fun depositPlayer(player: UUID, currency: String, amount: Double): EconomyResponse
+    suspend fun withdrawPlayer(player: UUID, currency: String, amount: Double): EconomyResponse
 
-    fun getBalance(player: UUID, currency: String): CompletableFuture<Double>
+    suspend fun getBalance(player: UUID, currency: String): Double
 
 }
